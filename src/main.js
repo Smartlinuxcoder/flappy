96b34f37691c25366eb645346e696318d05a7a80
Smartlinuxcoder <smartlinuxcoder@linuxmail.org>
2023-11-05 17:57:47 +0100

    ytfghgfyjtdf

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ const { default: kaboom } = require("kaboom")
 
 // Responding to gravity & jumping
 // Start kaboom
-var w = window.screen.width
-var h = window.screen.height
+var w = 412
+var h = 915
 /* var w = canvasWidth;
 var h = canvasHeight; */
 /* var w = window.innerWidth;
